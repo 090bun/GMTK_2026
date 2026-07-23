7ab727efe35bd2803910b35d361c577e38a28cee
bun <kasanomio@gmail.com>
2026-07-23 21:30:02 +0800

    能量補充道具

---
+++ Assets/Script/Porp_energy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Porp_energy : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private float Energy = 10f;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            playerController.AddEnergy(Energy);
+        }
+    }
+}
